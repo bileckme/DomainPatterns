b6c42c809d9780a6df9de461449d674feeda9e11
Biyi Akinpelu <biyi@dotslash.co.za>
2017-05-26 15:19:08 +0200

    updating composer

---
+++ src/Domain/Patterns/Behavioural/Strategy/Client.php
@@ -0,0 +1,28 @@
+<?php namespace Domain\Patterns\Tests\Behavioural\Strategy;
+
+use Domain\Patterns\Behavioural\Strategy\OutputInterface;
+
+/**
+ * Class Client
+ * @package Domain\Patterns\Tests\Behavioural\Strategy
+ */
+class Client
+{
+  private $output;
+
+  /**
+   * @param OutputInterface $outputType
+   */
+  public function setOutput(OutputInterface $outputType)
+  {
+    $this->output = $outputType;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function loadOutput()
+  {
+    return $this->output->load();
+  }
+}
